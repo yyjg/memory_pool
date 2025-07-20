5c984afcb3bbd1ab3dfef6efd027315682e780e2
JGYao <3170860094@qq.com>
2025-07-20 16:59:49 +0800

    delete build file

--- build/CMakeFiles/perf.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/perf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/perf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/perf.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for perf.
-# This may be replaced when dependencies are built.
